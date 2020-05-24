4b26380040c1951022664be4a070306ffbbd0df4
Anton Rodionov <anton.rodionov.ex@gmail.com>
2020-05-24 12:51:21 -0400

    Experimental dotnet support (#7)
    * dotnet init
    
    * make dotnet better
    
    * make dotnet test

---
+++ example_dotnet/app_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! 2 + 2 = " + Math.Sum(2, 2));
+        }
+    }
+}
